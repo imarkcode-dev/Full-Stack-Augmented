77945c9d53c3db27321cc69320ca549431d41f06
Marcos Magaña <marcosmh@outlook.com>
2026-05-08 23:27:57 -0600

    add respository

--- 05-06-phase/smart-billing/src/main/java/com/smart/billing/app/repository/AuthRepository.java
+++ 05-06-phase/smart-billing/src/main/java/com/smart/billing/app/repository/AuthRepository.java
@@ -3,9 +3,11 @@ package com.smart.billing.app.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.smart.billing.app.domain.LoginUser;
 
+@Repository
 public interface AuthRepository extends JpaRepository<LoginUser, Integer> {
 
    Optional<LoginUser> findByEmail(String email);
