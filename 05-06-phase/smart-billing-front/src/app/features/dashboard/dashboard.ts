bc0de960005ee373f2f015f9b027c6ca9297572e
Marcos Magaña <marcosmh@outlook.com>
2026-05-11 21:21:20 -0600

    added button customer 2

--- 05-06-phase/smart-billing-front/src/app/features/dashboard/dashboard.ts
+++ 05-06-phase/smart-billing-front/src/app/features/dashboard/dashboard.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [CommonModule, MatCardModule, MatProgressBarModule, MatIconModule],
+  imports: [CommonModule, MatCardModule, MatProgressBarModule, MatIconModule, MatButtonModule, RouterModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
